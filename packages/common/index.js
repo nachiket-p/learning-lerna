9c51e0b4df171bacf13589dedc3ca37485fe1c0d
Nachiket Patel <patel.nachiket.r@gmail.com>
2019-07-28 19:10:50 +0530

    learning lerna

---
+++ packages/common/index.js
@@ -0,0 +1,8 @@
+function test(i) {
+  console.log(`Hello Common ${i}`)
+}
+
+module.exports = {
+  test,
+  hello: ()=>"hello"
+}
---
+++ packages/server/index.js
@@ -0,0 +1,4 @@
+const common = require('common');
+
+common.hello();
+common.test('Zzz');
